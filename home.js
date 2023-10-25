0423b6915d7699910a45d9d09cc5e89e3e11abff
PhuongDuyV <vuduyphuong1511@gmail.com>
2023-10-25 15:41:39 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,8 @@
+document.getElementById("box1").addEventListener("click", function() {
+    var newPageURL = "node1.html"; 
+    window.location.href = newPageURL;
+});
+document.getElementById("box2").addEventListener("click", function() {
+    var newPageURL = "node2.html"; 
+    window.location.href = newPageURL;
+});
