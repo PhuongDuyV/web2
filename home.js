c3bc033040925625dd754b1ddc89346dfba75757
PhuongDuyV <vuduyphuong1511@gmail.com>
2023-11-02 19:34:36 +0700

    Remove all files

--- home.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("box1").addEventListener("click", function() {
-    var newPageURL = "node1.html"; 
-    window.location.href = newPageURL;
-});
-document.getElementById("box2").addEventListener("click", function() {
-    var newPageURL = "node2.html"; 
-    window.location.href = newPageURL;
-});
